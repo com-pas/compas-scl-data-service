cbd70d276a02dcba29d43ad54c883d735271ad1d
Dennis Labordus <dennis.labordus@alliander.com>
2021-05-18 08:29:31 +0200

    Added some more tests.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

---
+++ repository-postgresql/src/test/java/org/lfenergy/compas/scl/data/repository/CompasDataPostgreSQLRepositoryTest.java
@@ -0,0 +1,39 @@
+package org.lfenergy.compas.scl.data.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.lfenergy.compas.scl.SCL;
+import org.lfenergy.compas.scl.data.model.SclType;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+class CompasDataPostgreSQLRepositoryTest {
+    @InjectMocks
+    private CompasDataPostgreSQLRepository repository;
+
+    @Test
+    public void findSCLByUUID_WhenCalled_ThenUnsupportedExceptionThrown() {
+        assertThrows(UnsupportedOperationException.class, () -> {
+            repository.findSCLByUUID(SclType.SCD, UUID.randomUUID());
+        });
+    }
+
+    @Test
+    public void create_WhenCalled_ThenUnsupportedExceptionThrown() {
+        assertThrows(UnsupportedOperationException.class, () -> {
+            repository.create(SclType.SCD, new SCL());
+        });
+    }
+
+    @Test
+    public void delete_WhenCalled_ThenUnsupportedExceptionThrown() {
+        assertThrows(UnsupportedOperationException.class, () -> {
+            repository.delete(SclType.SCD, UUID.randomUUID());
+        });
+    }
+}
