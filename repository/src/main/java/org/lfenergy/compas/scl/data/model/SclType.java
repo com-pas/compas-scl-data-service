6ffced6496ea3a9e1bd7cc084709cf6c66dcfcdb
Dennis Labordus <dennis.labordus@alliander.com>
2021-05-18 09:30:59 +0200

    Added the SCL Types
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- repository/src/main/java/org/lfenergy/compas/scl/data/model/SclType.java
+++ repository/src/main/java/org/lfenergy/compas/scl/data/model/SclType.java
@@ -5,5 +5,19 @@
 package org.lfenergy.compas.scl.data.model;
 
 public enum SclType {
-    SCD;
+    SSD("Substation Specification Description"),
+    IID("IED instance description"),
+    ICD("IED Capability Description"),
+    SCD("Substation Configuration Description"),
+    CID("Configured IED description");
+
+    private final String description;
+
+    SclType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
