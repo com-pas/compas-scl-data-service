059897a3bf5c77696c5631689c4e30e3ea8707e1
Dennis Labordus <dennis.labordus@alliander.com>
2021-05-18 08:29:31 +0200

    Added some more tests.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

---
+++ repository/src/test/java/org/lfenergy/compas/scl/data/repository/SclDataExceptionTest.java
@@ -0,0 +1,25 @@
+package org.lfenergy.compas.scl.data.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SclDataExceptionTest {
+    @Test
+    public void SclDataException_WhenCalledWithOnlyCause_ThenCauseCanBeRetrieved() {
+        Exception expected = new RuntimeException();
+        SclDataException exception = new SclDataException(expected);
+
+        assertEquals(expected, exception.getCause());
+    }
+
+    @Test
+    public void SclDataException_WhenCalledWithCauseAndMessage_ThenCauseAndMessageCanBeRetrieved() {
+        String message = "The message";
+        Exception expected = new RuntimeException();
+        SclDataException exception = new SclDataException(message, expected);
+
+        assertEquals(message, exception.getMessage());
+        assertEquals(expected, exception.getCause());
+    }
+}
