28564c9c32d541cc99c66ef8dc65a609436599fb
Dennis Labordus <dennis.labordus@alliander.com>
2021-05-18 14:50:33 +0200

    Adding tests.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

---
+++ repository-basex/src/test/java/org/lfenergy/compas/scl/data/basex/TcpPortUtil.java
@@ -0,0 +1,22 @@
+// SPDX-FileCopyrightText: 2021 Alliander N.V.
+//
+// SPDX-License-Identifier: Apache-2.0
+package org.lfenergy.compas.scl.data.basex;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public final class TcpPortUtil {
+    private TcpPortUtil() {
+    }
+
+    public static int getFreePortNumber() throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(0)) {
+            if (serverSocket != null && serverSocket.getLocalPort() > 0) {
+                return serverSocket.getLocalPort();
+            }
+        }
+        throw new IOException("Port is not available");
+    }
+
+}
