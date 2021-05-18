aba503a594c9720bb209eb738bd4fb2d96303638
Dennis Labordus <dennis.labordus@alliander.com>
2021-05-18 13:41:43 +0200

    Adding tests.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- repository/src/test/java/org/lfenergy/compas/scl/data/model/AbstractEnumTest.java
+++ repository/src/test/java/org/lfenergy/compas/scl/data/model/AbstractEnumTest.java
@@ -5,11 +5,13 @@ package org.lfenergy.compas.scl.data.model;
 
 import java.lang.reflect.InvocationTargetException;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 abstract class AbstractEnumTest {
     protected void enumCodeCoverage(Class<? extends Enum<?>> enumClass) {
         try {
             for (var o : (Enum<?>[]) enumClass.getMethod("values").invoke(null)) {
-                enumClass.getMethod("valueOf", String.class).invoke(null, o.name());
+                assertNotNull(enumClass.getMethod("valueOf", String.class).invoke(null, o.name()));
             }
         } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException exp) {
             throw new RuntimeException(exp);
