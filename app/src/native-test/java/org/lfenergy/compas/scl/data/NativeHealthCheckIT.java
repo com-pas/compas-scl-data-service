5a3b27d99841f20e6887f11e013e347df3bde5a3
Dennis Labordus <dennis.labordus@alliander.com>
2021-05-18 12:28:08 +0200

    Trying to get jacoco working for code coverage.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- app/src/native-test/java/org/lfenergy/compas/scl/data/NativeHealthCheckIT.java
+++ app/src/native-test/java/org/lfenergy/compas/scl/data/NativeHealthCheckIT.java
@@ -7,6 +7,6 @@
 //import io.quarkus.test.junit.NativeImageTest;
 //
 //@NativeImageTest
-//class NativeHealthCheckTest extends HealthCheckTest {
+//class NativeHealthCheckIT extends HealthCheckTest {
 //    // Execute the same tests but in native mode.
 //}
