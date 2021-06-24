1538011b195d6b20c098883de12008a5b0f00255
Dennis Labordus <dennis.labordus@alliander.com>
2021-06-24 10:46:13 +0200

    Use of CompasExtensionsManager from core and added some tests.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- repository/src/test/java/org/lfenergy/compas/scl/data/model/SclTypeTest.java
+++ repository/src/test/java/org/lfenergy/compas/scl/data/model/SclTypeTest.java
@@ -4,6 +4,7 @@
 package org.lfenergy.compas.scl.data.model;
 
 import org.junit.jupiter.api.Test;
+import org.lfenergy.compas.scl.extensions.TSclFileType;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -14,9 +15,25 @@ class SclTypeTest extends AbstractEnumTest {
     }
 
     @Test
-    void testDescription() {
+    void getDescription_WhenCalledForEveryType_ThenEveryTypeHasADescription() {
         for (var type : SclType.values()) {
             assertNotNull(type.getDescription());
+        }
+    }
+
+    @Test
+    void convert_WhenCalledForEveryType_ThenEveryTypeCanBeConvertedToTSclFileType() {
+        for (var type : SclType.values()) {
+            TSclFileType tSclFileType = TSclFileType.valueOf(type.toString());
+            assertNotNull(tSclFileType);
+        }
+    }
+
+    @Test
+    void convert_WhenCalledForEveryTSclFileType_ThenEveryTSclFileTypeCanBeConvertedToType() {
+        for (var tSclFileType : TSclFileType.values()) {
+            SclType type = SclType.valueOf(tSclFileType.toString());
+            assertNotNull(type);
         }
     }
 }
