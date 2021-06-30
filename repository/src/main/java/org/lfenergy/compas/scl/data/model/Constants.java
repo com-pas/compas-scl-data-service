31bc9a21bbb5890b96a423673fa93ceed7288d67
Dennis Labordus <dennis.labordus@alliander.com>
2021-06-30 11:39:26 +0200

    Added private constructor.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- repository/src/main/java/org/lfenergy/compas/scl/data/model/Constants.java
+++ repository/src/main/java/org/lfenergy/compas/scl/data/model/Constants.java
@@ -4,6 +4,9 @@
 package org.lfenergy.compas.scl.data.model;
 
 public class Constants {
+    private Constants() {
+    }
+
     public static final String SCL_NAMESPACE = "http://www.iec.ch/61850/2003/SCL";
     public static final String SDS_NAMESPACE = "https://www.lfenergy.org/compas/SclDataService";
 }
