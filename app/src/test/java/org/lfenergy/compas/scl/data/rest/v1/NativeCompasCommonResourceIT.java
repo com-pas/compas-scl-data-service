87fba1a98809cbb37ec6d924034d6d2b68e57362
Rob Tjalma <rob@tjalma.com>
2021-10-07 09:52:19 +0200

    Work after review
    Signed-off-by: Rob Tjalma <rob@tjalma.com>

---
+++ app/src/test/java/org/lfenergy/compas/scl/data/rest/v1/NativeCompasCommonResourceIT.java
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2021 Alliander N.V.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.lfenergy.compas.scl.data.rest.v1;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+class NativeCompasCommonResourceIT extends CompasCommonResourceTest {
+    // Execute the same tests but in native mode.
+}
