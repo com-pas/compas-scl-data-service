1b1889b5dcb41469ee65c24c4985c62f7425e1f4
Dennis Labordus <dennis.labordus@alliander.com>
2021-11-17 09:23:28 +0100

    Added Flyway and done some refactoring to make the projects work with both implementations.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- repository-postgresql/src/main/resources/org/lfenergy/compas/scl/data/repository/postgres/db/migration/V1_0__create_scl_file.sql
+++ repository-postgresql/src/main/resources/org/lfenergy/compas/scl/data/repository/postgres/db/migration/V1_0__create_scl_file.sql
@@ -1,7 +1,7 @@
 /**
-  SPDX-FileCopyrightText: 2021 Alliander N.V.
-
-  SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: 2021 Alliander N.V.
+ *
+ * SPDX-License-Identifier: Apache-2.0
 */
 
 --
