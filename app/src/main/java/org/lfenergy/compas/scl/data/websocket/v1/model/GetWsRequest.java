4e79a854de91630997025bac5a367e30f6de6b08
Dennis Labordus <dennis.labordus@alliander.com>
2022-11-10 15:31:22 +0100

    Small improvements.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- app/src/main/java/org/lfenergy/compas/scl/data/websocket/v1/model/GetWsRequest.java
+++ app/src/main/java/org/lfenergy/compas/scl/data/websocket/v1/model/GetWsRequest.java
@@ -6,6 +6,7 @@ package org.lfenergy.compas.scl.data.websocket.v1.model;
 
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
+import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.*;
 import java.util.UUID;
 
@@ -18,6 +19,7 @@ import static org.lfenergy.compas.scl.data.SclDataServiceConstants.SCL_DATA_SERV
 public class GetWsRequest {
     @Schema(description = "The ID of the SCL File.",
             example = "f7b98f4d-3fe4-4df2-8533-d7f0c1800344")
+    @NotNull
     @XmlElement(name = "Id", namespace = SCL_DATA_SERVICE_V1_NS_URI, required = true)
     private UUID id;
 
