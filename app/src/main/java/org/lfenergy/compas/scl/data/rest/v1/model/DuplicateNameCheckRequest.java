66b7784212dfbd3027fa97e9ebc906baa5ff2d30
Stef3st <steffen.van.den.driest@alliander.com>
2022-12-13 17:11:00 +0100

    Added header
    Signed-off-by: Stef3st <steffen.van.den.driest@alliander.com>

--- app/src/main/java/org/lfenergy/compas/scl/data/rest/v1/model/DuplicateNameCheckRequest.java
+++ app/src/main/java/org/lfenergy/compas/scl/data/rest/v1/model/DuplicateNameCheckRequest.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 Alliander N.V.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.lfenergy.compas.scl.data.rest.v1.model;
 
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
