2f34f9d188e037ed6d7dcc4fdcf8fd2e7184ba33
Berke Akkaya <berke.akkaya@bearingpoint.com>
2026-02-24 14:06:49 +0100

    fix: Update InjectMock import for Quarkus 3.31 compatibility

--- app/src/test/java/org/lfenergy/compas/scl/data/websocket/v1/CompasSclGetServerEndpointAsReaderTest.java
+++ app/src/test/java/org/lfenergy/compas/scl/data/websocket/v1/CompasSclGetServerEndpointAsReaderTest.java
@@ -5,7 +5,7 @@ package org.lfenergy.compas.scl.data.websocket.v1;
 
 import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
-import io.quarkus.test.junit.mockito.InjectMock;
+import io.quarkus.test.InjectMock;
 import org.junit.jupiter.api.Test;
 import org.lfenergy.compas.scl.data.service.CompasSclDataService;
 import org.lfenergy.compas.scl.data.websocket.v1.decoder.GetWsResponseDecoder;
