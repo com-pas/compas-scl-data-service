4cae00c21f69aa7a88c787aa9cf2c87e575b527e
Stiliyan Valkanov <stiliyan.valkanov@bearingpoint.com>
2026-04-14 09:38:19 +0200

    fix: remove unused import
    Signed-off-by: Stiliyan Valkanov <stiliyan.valkanov@bearingpoint.com>

--- api/src/main/java/org/lfenergy/compas/scl/data/rest/dto/DataEntry.java
+++ api/src/main/java/org/lfenergy/compas/scl/data/rest/dto/DataEntry.java
@@ -6,7 +6,6 @@ package org.lfenergy.compas.scl.data.rest.dto;
 import java.util.Date;
 import java.util.UUID;
 import jakarta.validation.constraints.*;
-import jakarta.validation.Valid;
 
 import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonProperty;
