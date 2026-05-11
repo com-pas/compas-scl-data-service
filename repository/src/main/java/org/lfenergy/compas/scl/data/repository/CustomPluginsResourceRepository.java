58156ce36e1b8aba68741cd479d4cdb19fd2dff6
Stiliyan Valkanov <stiliyan.valkanov@bearingpoint.com>
2026-05-11 11:32:42 +0200

    chore: added missing licenses
    Signed-off-by: Stiliyan Valkanov <stiliyan.valkanov@bearingpoint.com>

--- repository/src/main/java/org/lfenergy/compas/scl/data/repository/CustomPluginsResourceRepository.java
+++ repository/src/main/java/org/lfenergy/compas/scl/data/repository/CustomPluginsResourceRepository.java
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2026 BearingPoint GmbH
+//
+// SPDX-License-Identifier: Apache-2.0
 package org.lfenergy.compas.scl.data.repository;
 
 import io.quarkus.hibernate.panache.managed.blocking.PanacheManagedBlockingRepositoryBase;
